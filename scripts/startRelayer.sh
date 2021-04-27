4cc9145f4ca2421fafef6b162511d0bd0db76026
Saad Ahmed <saad.sengr@gmail.com>
2021-04-28 03:02:45 +0500

    More script updates

--- scripts/startRelayer.sh
+++ scripts/startRelayer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd env; . relayer.env;
 cd ../relayer;
-docker run -e KEYSTORE_PASSWORD=$KEYSTORE_PASSWORD -v `pwd`/config.json:/config.json -v `pwd`/keys:/keys chainsafe/chainbridge
+sudo make build;
+export KEYSTORE_PASSWORD=$KEYSTORE_PASSWORD
+./build/chainbridge --config config.json --verbosity trace --latest

acacf437c975bf91efbd8b38d96305efb26b91e0
Saad Ahmed <saad.sengr@gmail.com>
2021-04-28 15:00:39 +0500

    Docker should start fine.

--- cli/relayer.js
+++ cli/relayer.js
@@ -9,48 +9,35 @@ exports.startRelayerCmd = new commander.Command("setupRelayer")
     .description("Start a relayer")
     .action(async function (args) {
 
-        inquirer
-            .prompt([
+        let configToWrite = fs.readFileSync(path.join(__dirname, `../deployments/${process.env.BRIDGE}.json`), { encoding: 'utf-8'});
+        configToWrite = JSON.parse(configToWrite);
+
+        configToWrite.chains[0].from = cfg.parsed.CH1_ADDR;
+        configToWrite.chains[1].from = cfg.parsed.CH2_ADDR;
+
+        if (!configToWrite.chains[0].endpoint.length) {
+            let ans = await inquirer.prompt([
                 {
-                    type: 'list',
-                    name: 'bridge',
-                    message: 'Select a bridge: ',
-                    choices: bridgeConfigs.bridges.map((bridge, index) => {
-                        return `${index + 1}. ${bridge.chains[0].name} - ${bridge.chains[1].name}`;
-                    }),
-                },
+                    name: 'endpoint',
+                    message: `[${configToWrite.chains[0].name}] RPC Url: `
+                }
             ])
-            .then(async (answers) => {
-                let chainIndex = Number(answers.bridge.split(".")[0]) - 1;
-                let configToWrite = bridgeConfigs.bridges[chainIndex];
 
-                configToWrite.chains[0].from = cfg.parsed.CH1_ADDR;
-                configToWrite.chains[1].from = cfg.parsed.CH2_ADDR;
+            configToWrite.chains[0].endpoint = ans.endpoint;
+            if (ans.endpoint.startsWith('http')) configToWrite.chains[0].opts['http'] = 'true';
+        }
 
-                if (!configToWrite.chains[0].endpoint.length) {
-                    let ans = await inquirer.prompt([
-                        {
-                            name: 'endpoint',
-                            message: `[${configToWrite.chains[0].name}] RPC Url: `
-                        }
-                    ])
-
-                    configToWrite.chains[0].endpoint = ans.endpoint;
-                    if (ans.endpoint.startsWith('http')) configToWrite.chains[0].opts['http'] = 'true';
+        if (!configToWrite.chains[1].endpoint.length) {
+            let ans = await inquirer.prompt([
+                {
+                    name: 'endpoint',
+                    message: `[${configToWrite.chains[1].name}] RPC Url: `
                 }
+            ])
 
-                if (!configToWrite.chains[1].endpoint.length) {
-                    let ans = await inquirer.prompt([
-                        {
-                            name: 'endpoint',
-                            message: `[${configToWrite.chains[1].name}] RPC Url: `
-                        }
-                    ])
+            configToWrite.chains[1].endpoint = ans.endpoint;
+            if (ans.endpoint.startsWith('http')) configToWrite.chains[1].opts['http'] = 'true';
+        }
 
-                    configToWrite.chains[1].endpoint = ans.endpoint;
-                    if (ans.endpoint.startsWith('http')) configToWrite.chains[1].opts['http'] = 'true';
-                }
-
-                fs.writeFileSync(path.join(__dirname, '../relayer/') + `config.json`, JSON.stringify(configToWrite), 'utf-8');
-            });
-    });
+        fs.writeFileSync(path.join(__dirname, '../relayer/') + `config.json`, JSON.stringify(configToWrite), 'utf-8');
+});
--- scripts/runDocker.sh
+++ scripts/runDocker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd env; . relayer.env;
 cd ../relayer;
-docker run  --restart=always -e KEYSTORE_PASSWORD=$KEYSTORE_PASSWORD -v `pwd`/config.json:/config.json -v `pwd`/keys/:/keys/ chainsafe/chainbridge
+docker run  --restart=always -e KEYSTORE_PASSWORD=$KEYSTORE_PASSWORD -v `pwd`/config.json:/config.json -v `pwd`/keys/:/keys/ chainsafe/chainbridge:1.1.1  --verbosity trace --latest
